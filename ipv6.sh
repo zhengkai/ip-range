424accc688d494ef7ff28fa680aaac5638657f92
Zheng Kai <zhengkai@gmail.com>
2024-12-27 16:22:53 +0800

    ipv6

--- ipv6.sh
+++ ipv6.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
-FILE_DATA="step1/v4-data.txt"
-FILE_COUNTRY="step1/v4-country.txt"
+FILE_DATA="step1/v6-data.txt"
+FILE_COUNTRY="step1/v6-country.txt"
 
 cd "$(dirname "$0")" || exit 1
 
 mkdir -p step1
 
 echo "Generating IPv4 list"
-grep -h '|ipv4|' raw/*.txt | grep -v -E "\|\||\|\*\|" > "$FILE_DATA"
+grep -h '|ipv6|' raw/*.txt | grep -v -E "\|\||\|\*\|" > "$FILE_DATA"
 
 echo "Generating country list"
 cut -d'|' -f2 "$FILE_DATA" | sort -u > "$FILE_COUNTRY"
@@ -23,10 +23,10 @@ while IFS= read -r line; do
 done < "$FILE_COUNTRY"
 echo "OK"
 
-mkdir -p step2
+mkdir -p step-v6
 
 while IFS= read -r C; do
 	echo -n "$C "
-	grep "|${C}|" "$FILE_DATA" | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' \
-		> "step2/${C,,}.txt"
+	grep "|${C}|" "$FILE_DATA" | awk -F'|' '/\|US\|ipv6\|/ { print $4 "/" $5 }' "$FILE_DATA" \
+		> "step-v6/${C,,}.txt"
 done < "$FILE_COUNTRY"
